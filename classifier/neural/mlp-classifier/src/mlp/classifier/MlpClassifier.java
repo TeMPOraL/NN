e73a10a4638c676af6978405a39f4b098a4a9ba2
Jacek Zlydach <temporal.pl@gmail.com>
2012-09-25 01:05:08 +0200

    Some skeleton code :).

--- classifier/neural/mlp-classifier/src/mlp/classifier/MlpClassifier.java
+++ classifier/neural/mlp-classifier/src/mlp/classifier/MlpClassifier.java
@@ -12,11 +12,10 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author TeMPOraL
@@ -30,15 +29,19 @@ public class MlpClassifier {
         InetSocketAddress addr = new InetSocketAddress(8080);
         HttpServer server = HttpServer.create(addr, 0);
         
-        server.createContext("/", new ClassifierRequestHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/getTimetables", new GetTimetablesRequestHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/deleteTimetable", new DeleteTimetableRequestHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/rememberTimetable", new RememberTimetableRequestHandler()).getFilters().add(new ParameterFilter());
         server.setExecutor(Executors.newCachedThreadPool());
         server.start();
         System.out.println("Server is listening on port 8080" );
     }
 }
 
-class ClassifierRequestHandler implements HttpHandler {
+abstract class ClassifierRequestHandler implements HttpHandler {
 
+    public abstract void execute(OutputStream out, Map<String, Object> params);
+        
     @Override
     public void handle(HttpExchange he) throws IOException {
         Headers responseHeaders = he.getResponseHeaders();
@@ -46,26 +49,52 @@ class ClassifierRequestHandler implements HttpHandler {
         he.sendResponseHeaders(200, 0);
         
         OutputStream responseBody = he.getResponseBody();
-        responseBody.write("Hello world!\n".getBytes());
-        
-        Headers requestHeaders = he.getRequestHeaders();
-        Set<String> keySet = requestHeaders.keySet();
         
         Map<String, Object> params = (Map<String, Object>) he.getAttribute("parameters");
-        
-        Iterator<String> iter = keySet.iterator();
-        while (iter.hasNext()) {
-            String key = iter.next();
-            List values = requestHeaders.get(key);
-            String s = key + " = " + values.toString() + "\n";
-            responseBody.write(s.getBytes());
-        }
-        responseBody.write((he.getRequestURI().toString() + "\n").getBytes());
 
-        for(Map.Entry<String, Object> param : params.entrySet()) {
+       for(Map.Entry<String, Object> param : params.entrySet()) {
             responseBody.write( ("" + param.getKey() + ": " + (String)param.getValue() + "\n").getBytes());
         }
+       
+       this.execute(responseBody, params);
+       
         responseBody.close();
         
+    }
+}
+
+class GetTimetablesRequestHandler extends ClassifierRequestHandler {
+    
+    @Override
+    public void execute(OutputStream out, Map<String, Object> params) {
+        try {
+            out.write("GetTimetablesRequest\n".getBytes());
+        } catch (IOException ex) {
+            Logger.getLogger(ClassifierRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+class DeleteTimetableRequestHandler extends ClassifierRequestHandler {
+    
+    @Override
+    public void execute(OutputStream out, Map<String, Object> params) {
+        try {
+            out.write("DeleteTimetableRequest\n".getBytes());
+        } catch (IOException ex) {
+            Logger.getLogger(ClassifierRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+class RememberTimetableRequestHandler extends ClassifierRequestHandler {
+    
+    @Override
+    public void execute(OutputStream out, Map<String, Object> params) {
+        try {
+            out.write("RememberTimetableRequest\n".getBytes());
+        } catch (IOException ex) {
+            Logger.getLogger(ClassifierRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- classifier/neural/mlp-classifier/src/mlp/classifier/NNClassifier_MLP.java
+++ classifier/neural/mlp-classifier/src/mlp/classifier/NNClassifier_MLP.java
@@ -4,11 +4,23 @@
  */
 package mlp.classifier;
 
+import org.neuroph.nnet.MultiLayerPerceptron;
+
 /**
  *
  * @author TeMPOraL
  */
 public class NNClassifier_MLP implements Classifier {
+    
+    public final static int INPUTS = 3;
+    public final static int OUTPUTS = 20;
+    public final static int HIDDEN = 5;
+    
+    private MultiLayerPerceptron nnet;
+    
+    NNClassifier_MLP() {
+        nnet = new MultiLayerPerceptron(INPUTS, HIDDEN, OUTPUTS);
+    }
 
     @Override
     public void learn() {
